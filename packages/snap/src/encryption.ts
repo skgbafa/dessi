5714afcbb79aee24870b359f82e1432ae3880d14
Samuel Gbafa <skgbafa@gmail.com>
2022-10-29 20:59:22 +0100

    eth_getEncryptionPublicKey test passing

--- packages/snap/src/encryption.ts
+++ packages/snap/src/encryption.ts
@@ -1,6 +1,23 @@
-import { keccak256 } from '@ethersproject/keccak256';
+import keccak256 from 'keccak256';
 import secp256k1 from 'secp256k1';
-import { decrypt } from 'eciesjs';
+import { decrypt, utils } from 'eciesjs';
+
+// Implements the X9.63 key derivation function as described in
+// https://www.secg.org/sec1-v2.pdf#subsubsection.3.6.1
+// From https://github.com/PatriceVignola/eip-5630/blob/master/eip5630.ts#L12
+const X963KDF = (address: Uint8Array, secretSigningKey: Uint8Array): Buffer => {
+  const counter = Buffer.allocUnsafe(4);
+  counter.writeUInt32BE(1, 0);
+  const keyData = Buffer.concat([secretSigningKey, counter, address]);
+  return keccak256(keyData);
+};
+
+const publicKeyToAddress = (publicKeyBuffer: Uint8Array): string => {
+  const address = `0x${keccak256(Buffer.from(publicKeyBuffer.slice(1)))
+    .subarray(-20)
+    .toString('hex')}`;
+  return address;
+};
 
 const getPrivateKeyFromAccount = (account: string) => {
   // get private key from account
@@ -13,7 +30,12 @@ const getPrivateKeyFromAccount = (account: string) => {
 const getEncryptionKeyFromAccount = (account: string) => {
   const privateKey = getPrivateKeyFromAccount(account);
   // get encryption key from private key
-  return Buffer.from('', 'hex');
+  const privateKeyBuffer = Buffer.from(utils.remove0x(privateKey), 'hex');
+  const publicKeyBuffer = secp256k1.publicKeyCreate(privateKeyBuffer, false);
+  const address = publicKeyToAddress(publicKeyBuffer);
+  const addressBuffer = Buffer.from(address, 'hex');
+  const encryptionKey = X963KDF(addressBuffer, privateKeyBuffer);
+  return encryptionKey;
 };
 
 const eth_getEncryptionPublicKey = (account: string) => {
