b755dae10954ec75a0c1275dae455b8e4ccaa3a3
Samuel Gbafa <skgbafa@gmail.com>
2022-10-29 14:52:42 +0100

    rpc functions

---
+++ packages/snap/src/encryption.ts
@@ -0,0 +1,10 @@
+/* eslint-disable camelcase */
+const eth_getEncryptionPublicKey = (account) => {
+  return '';
+};
+
+const eth_decrypt = (encryptedMessage, account) => {
+  return '';
+};
+
+export { eth_getEncryptionPublicKey, eth_decrypt };
