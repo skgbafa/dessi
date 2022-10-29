d9e44d0b174e21aafc80e028a16552de55afb228
Samuel Gbafa <skgbafa@gmail.com>
2022-10-29 19:34:49 +0100

    rough idea

--- packages/snap/src/encryption.ts
+++ packages/snap/src/encryption.ts
@@ -1,5 +1,20 @@
+import { keccak256 } from '@ethersproject/keccak256';
+
+const getPrivateKeyFromAccount = (account: string) => {
+  // get private key from account
+  return '';
+};
+
+const getEncryptionKeyFromAccount = (account: string) => {
+  const privateKey = getPrivateKeyFromAccount(account);
+  // get encryption key from private key
+  return '';
+};
+
 const eth_getEncryptionPublicKey = (account: string) => {
   console.log('eth_getEncryptionPublicKey');
+  const encryptionKey = getEncryptionKeyFromAccount(account);
+  // get public key from encryption key
   return '';
 };
 
@@ -8,6 +23,8 @@ const eth_decrypt = (
   account: string,
 ) => {
   console.log('eth_decrypt');
+  const encryptionKey = getEncryptionKeyFromAccount(account);
+  // decrypt message
   return '';
 };
 
